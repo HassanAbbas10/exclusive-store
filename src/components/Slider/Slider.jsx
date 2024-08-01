cd946b7e0618e6e72c77130572af2b0577555849
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2024-08-01 17:11:50 +0500

    modified:   src/components/Slider/Slider.jsx

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -15,16 +15,16 @@ const ImageSlider = () => {
       };
     
       return (
-        <div className="sm:w-[55rem] w-full mx-auto sm:mx-5 h-[20rem] sm:p-5 ">
+        <div className="sm:w-[55rem] w-full mx-auto sm:mx-5 my-10 sm:h-[20rem] sm:p-5 ">
           <Slider {...settings}>
             <div>
-              <img src="https://th.bing.com/th/id/OIP.AOKJJ986l7bnttZ7uat_sQHaHa?rs=1&pid=ImgDetMain"  alt="Slide 1" className="w-[50rem] h-[30rem] object-fit" />
+              <img src="https://th.bing.com/th/id/OIP.AOKJJ986l7bnttZ7uat_sQHaHa?rs=1&pid=ImgDetMain"  alt="Slide 1" className="w-[50rem] sm:h-[30rem] h-[25rem] object-fit" />
             </div>
             <div>
-              <img src="https://via.placeholder.com/800x400" alt="Slide 2" className="w-[50rem] h-[30rem] object-fit"/>
+              <img src="https://th.bing.com/th/id/R.05a816a28c9dab57fe191b2be9f3e30e?rik=5WQnzlQj%2b63xuA&pid=ImgRaw&r=0" alt="Slide 2" className="w-[50rem] sm:h-[30rem] h-[25rem] object-fit"/>
             </div>
             <div>
-              <img src="https://via.placeholder.com/800x400" alt="Slide 3" className="w-[50rem] h-[30rem] object-fit" />
+              <img src="https://cdn.shopify.com/s/files/1/3103/8852/products/4_Piece_Canvas_Modern_Abstract_patterns_6f121d35-500b-4a7b-aefa-bdcd1a54d208.jpg?v=1570037115" alt="Slide 3" className="w-[50rem] sm:h-[30rem] h-[25rem] object-fit" />
             </div>
           </Slider>
         </div>
